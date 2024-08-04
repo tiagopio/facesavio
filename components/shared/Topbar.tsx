4a747478b6d3307ceeb095baa17c881a9b60417d
tiago <97364045+tiagopio@users.noreply.github.com>
2024-08-04 13:16:25 -0300

    Update Topbar.tsx

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -40,7 +40,6 @@ async function Topbar() {
           </SignedIn>
         </div>
 
-        <UserButton />
         <Tooltip>
           <TooltipTrigger asChild>
             <Link href={`/profile/${user.username}`}>
